968869d888010df797d68fd0413d5d2527d7d312
KameswaranS <KameswaranS@hexaware.com>
2015-05-20 17:27:15 +0530

    first commit

---
+++ student-app/student/app/student.app.js
@@ -0,0 +1,24 @@
+﻿
+var studentApp = angular.module('studentApp', ['ngRoute', 'createControllers']);
+
+studentApp.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/', {
+            redirectTo: '/login'
+        })
+        .when('/login', {
+            templateUrl: function ($routeParams) {
+                return '../login.html';
+            }
+        })
+        .when('/:dir/:page', {
+            templateUrl: function ($routeParams) {                
+                return '../' + $routeParams.dir + '/views/' + $routeParams.page + '.html';
+            }
+        })
+        .otherwise({
+
+        });
+
+}]);
---
+++ student-app/student/controllers/student/create-controller.js
@@ -0,0 +1,10 @@
+﻿var createControllers = angular.module('createControllers', [])
+
+createControllers.controller('createCtrl', ['$scope', function ($scope) {
+
+    $scope.Create = function () {        
+        if ($scope.frmEntry.$valid)
+            alert('Added ' + $scope.studentName);
+    };
+
+}]);
